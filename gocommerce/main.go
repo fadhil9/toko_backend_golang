3be3dd11b4bda6125a6a079d3c1b258b12db7a96
fadhil9 <fadhilmuhamad272@gmail.com>
2024-07-23 13:48:44 +0700

    update

--- gocommerce/main.go
+++ gocommerce/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"main.go/configs"
 	"main.go/handlers"
+	"main.go/middlewares"
 )
 
 func main() {
@@ -17,11 +18,23 @@ func main() {
 	router := gin.Default()
 
 	// Rute CRUD Produk
-	router.GET("/products", handlers.ListProducts(db))
+	router.GET("/products", middlewares.AuthMiddleware(), handlers.ListProducts(db))
 	router.GET("/products/:id", handlers.GetProduct(db))
 	router.POST("/products", handlers.CreateProduct(db))
 	router.PUT("/products/:id", handlers.UpdateProduct(db))
 	router.DELETE("/products/:id", handlers.DeleteProduct(db))
+
+	// Rute CRUD Product Category
+	router.GET("/product-categories", handlers.ListProductCategories(db))
+	router.GET("/product-categories/:id", handlers.GetProductCategory(db))
+	router.POST("/product-categories", handlers.CreateProductCategory(db))
+	router.PUT("/product-categories/:id", handlers.UpdateProductCategory(db))
+	router.DELETE("/product-categories/:id", handlers.DeleteProductCategory(db))
+
+	//transaction
+	//login register
+	router.POST("/login", handlers.Login(db))
+	router.POST("/register", handlers.Register(db))
 
 	router.Run(":8080")
 
